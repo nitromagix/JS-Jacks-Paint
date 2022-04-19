55a9aa5e1c1045c3b41fe095a2d4498243f09163
Martin <nitromagix@gmail.com>
2022-04-18 20:35:28 -0700

    moved product data to HTML page (using custom attributes on img elements)

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -25,49 +25,8 @@ function removeOpacity(event) {
 
 function getProductInfo(element) {
 
-   let elementId = element.id
-   let price;
-   let colorName;
-
-   switch (elementId) {
-      case 'pn1':
-         price = '$14.99';
-         colorName = 'Lime Green';
-         break;
-      case 'pn2':
-         price = '$11.14';
-         colorName = 'Medium Brown';
-         break;
-      case 'pn3':
-         price = '$22.99';
-         colorName = 'Royal Blue';
-         break;
-      case 'pn4':
-         price = '$13.42';
-         colorName = 'Solid Red';
-         break;
-      case 'pn5':
-         price = '$21.98';
-         colorName = 'Solid White';
-         break;
-      case 'pn6':
-         price = '$24.99';
-         colorName = 'Solid Black';
-         break;
-      case 'pn7':
-         price = '$8.22';
-         colorName = 'Solid Cyan';
-         break;
-      case 'pn8':
-         price = '$11.99';
-         colorName = 'Solid Purple';
-         break;
-      case 'pn9':
-         price = '$14.99';
-         colorName = 'Solid Yellow';
-         break;
-      default:
-   }
+   let price = element.getAttribute("price");
+   let colorName = element.getAttribute('name');
 
    let colorNameElement = document.querySelector('#color-name');
    let colorPriceElement = document.querySelector('#color-price');
